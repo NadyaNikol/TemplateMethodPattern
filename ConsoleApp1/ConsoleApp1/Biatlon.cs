dacd30a88121221749014147fa6078520e08b4ff
NadyaNikol <nikolaenkonadin@gmail.com>
2019-06-06 17:52:28 +0300

    ..

---
+++ ConsoleApp1/ConsoleApp1/Biatlon.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+     abstract class BiatlonCalculation
+    {
+        int sec = 0;
+        int point = 0;
+        bool hit = false;
+        public void Game()
+        {
+            Running(sec, point);
+            Shooting(point, hit);
+        }
+
+
+        public abstract void Running(int sec, int point);
+
+        public abstract void Shooting(int point, bool hit);
+    }
+
+    class ManCalculation : BiatlonCalculation
+    {
+        public override void Running(int sec, int point)
+        {
+            point -= 5;
+            sec++;
+        }
+
+        public override void Shooting(int point, bool hit)
+        {
+            if (hit) { point += 100; }
+        }
+    }
+
+    //class WomanCalculation : BiatlonCalculation
+    //{
+
+    //}
+
+    //class ChildrenCalculation : BiatlonCalculation
+    //{
+
+
+    //}
+}
